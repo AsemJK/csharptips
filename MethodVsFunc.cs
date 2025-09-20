b021fe5e3d2a9a92ac188ab90cc0c69d31e2a200
asemjk <asemalsaiyadi@gmail.com>
2025-09-20 05:55:40 +0300

    Add project files.

---
+++ MethodVsFunc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharptips
+{
+    internal class MethodVsFunc
+    {
+        public void Run()
+        {
+            Func<int, int> func = x => x * x;
+            int resultFunc = func(5);
+            Console.WriteLine($"Func result: {resultFunc}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharptips;
+
+Console.WriteLine("Hello, World!");
+
+//Method vs Func example
+MethodVsFunc methodVsFunc = new MethodVsFunc();
+methodVsFunc.Run();
